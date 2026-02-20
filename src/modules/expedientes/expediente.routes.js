ce03ba41b5bfdb635ba61fc807d98a90335f33f8
Wilmer1404 <samirloroamaya24@gmail.com>
2026-02-20 03:38:35 -0500

    se agrega el verificador de token

--- src/modules/expedientes/expediente.routes.js
+++ src/modules/expedientes/expediente.routes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const expedienteController = require('./expediente.controller');
 const uploadMiddleware = require('../../middlewares/upload.middleware');
+const { verificarToken } = require('../../middlewares/auth.middleware');
 
-// Esperamos que el archivo venga en el campo "archivo_adjunto"
-router.post('/nuevo', uploadMiddleware.single('archivo_adjunto'), expedienteController.crearNuevoExpediente);
+// Fíjate en el orden: 1° Verifica Token -> 2° Sube el archivo -> 3° Ejecuta el controlador
+router.post('/nuevo', 
+    verificarToken, 
+    uploadMiddleware.single('archivo_adjunto'), 
+    expedienteController.crearNuevoExpediente
+);
 
 module.exports = router;
