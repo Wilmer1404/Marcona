35f9ffc38d4eba7503b8e59960d50761ed3f8f8d
Wilmer1404 <samirloroamaya24@gmail.com>
2026-02-20 02:24:41 -0500

    Agrega la función para insertar el usuario en PostgreSQL:

--- src/modules/usuarios/usuario.service.js
+++ src/modules/usuarios/usuario.service.js
@@ -1,16 +1,27 @@
 const { pool } = require('../../config/database');
 
 const obtenerTodos = async () => {
-    // Traemos los datos básicos sin exponer la contraseña
-    const query = `
-        SELECT id, dni, nombres, apellidos, correo, rol, activo 
-        FROM usuarios 
-        ORDER BY id ASC
-    `;
+    const query = `SELECT id, dni, nombres, apellidos, correo, rol, activo FROM usuarios ORDER BY id ASC`;
     const { rows } = await pool.query(query);
     return rows;
 };
 
+//Insertar usuario
+const crearUsuario = async (datosUsuario) => {
+    const { dni, nombres, apellidos, correo, password_hash, departamento_id, rol } = datosUsuario;
+    
+    const query = `
+        INSERT INTO usuarios (dni, nombres, apellidos, correo, password_hash, departamento_id, rol)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id, dni, nombres, correo, rol;
+    `;
+    
+    const values = [dni, nombres, apellidos, correo, password_hash, departamento_id, rol];
+    const { rows } = await pool.query(query, values);
+    return rows[0]; // Retorna el usuario recién creado (sin la contraseña)
+};
+
 module.exports = {
-    obtenerTodos
+    obtenerTodos,
+    crearUsuario
 };
